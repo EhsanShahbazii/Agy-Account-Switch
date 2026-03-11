607a1f917d339787f381ee2ab9303d39f68643ab
EhsanShahbazii <ehsan.shahbazipc@gmail.com>
2026-03-11 10:05:45 +0330

    fix(keychain): handle empty keychain item and missing attributes safely

--- src/core/keychain.js
+++ src/core/keychain.js
@@ -1,11 +1,11 @@
 "use strict";
-const { execSync } = require("child_process");
+const { execFileSync } = require("child_process");
 const { KEYCHAIN_SERVICE, KEYCHAIN_ACCOUNT } = require("../utils/paths");
 
 class KeychainHelper {
     static getPassword(service = KEYCHAIN_SERVICE, account = KEYCHAIN_ACCOUNT) {
         try {
-            const out = execSync(`security find-generic-password -s "${service}" -a "${account}" -w`, {
+            const out = execFileSync("security", ["find-generic-password", "-s", service, "-a", account, "-w"], {
                 encoding: "utf-8",
                 stdio: ["ignore", "pipe", "ignore"]
             });
@@ -17,7 +17,18 @@ class KeychainHelper {
 
     static setPassword(password, service = KEYCHAIN_SERVICE, account = KEYCHAIN_ACCOUNT) {
         try {
-            execSync(`security add-generic-password -U -s "${service}" -a "${account}" -w "${password.replace(/"/g, '\\"')}"`, {
+            execFileSync("security", ["add-generic-password", "-U", "-s", service, "-a", account, "-w", password], {
+                stdio: "ignore"
+            });
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }
+
+    static deletePassword(service = KEYCHAIN_SERVICE, account = KEYCHAIN_ACCOUNT) {
+        try {
+            execFileSync("security", ["delete-generic-password", "-s", service, "-a", account], {
                 stdio: "ignore"
             });
             return true;
