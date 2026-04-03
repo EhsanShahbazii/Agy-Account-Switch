820ce9fae37e2dd3926023b622eb21ea36798948
EhsanShahbazii <ehsan.shahbazipc@gmail.com>
2026-04-03 13:15:33 +0330

    feat(injector): add automatic backup of app.asar before patch

--- src/injector/patcher.js
+++ src/injector/patcher.js
@@ -5,6 +5,16 @@ const paths = require("../utils/paths");
 const logger = require("../utils/logger");
 
 class Patcher {
+    static backupAsar(asarPath = paths.ASAR_PATH, backupPath = paths.ASAR_BACKUP_PATH) {
+        if (!fs.existsSync(backupPath)) {
+            logger.info(`Creating clean backup at ${backupPath}...`);
+            fs.copyFileSync(asarPath, backupPath);
+            logger.success("Backup successfully created.");
+        } else {
+            logger.info(`Backup already exists at ${backupPath}`);
+        }
+    }
+
     static unpackAsar(asarPath, destDir) {
         logger.info(`Unpacking ${asarPath} to ${destDir}...`);
         execSync(`npx @electron/asar extract "${asarPath}" "${destDir}"`, { stdio: "inherit" });
