9484d3999f24d1635e63552c0921e2b452ac2776
EhsanShahbazii <ehsan.shahbazipc@gmail.com>
2026-04-10 10:25:30 +0330

    feat(ipc): register accounts:add, accounts:remove, and accounts:rename handlers

--- src/ipc/ipcHandlers.js
+++ src/ipc/ipcHandlers.js
@@ -11,6 +11,18 @@ function registerAccountIpcHandlers(ipcMain) {
     ipcMain.handle("accounts:switch", async (_event, accountId) => {
         return await accountManager.switchAccount(accountId);
     });
+
+    ipcMain.handle("accounts:add", async (_event, label, tokenStr) => {
+        return await accountManager.addAccount(label, tokenStr);
+    });
+
+    ipcMain.handle("accounts:remove", async (_event, accountId) => {
+        return await accountManager.removeAccount(accountId);
+    });
+
+    ipcMain.handle("accounts:rename", async (_event, accountId, newLabel) => {
+        return await accountManager.renameAccount(accountId, newLabel);
+    });
 }
 
 module.exports = { registerAccountIpcHandlers };
