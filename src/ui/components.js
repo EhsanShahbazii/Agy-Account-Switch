ea7c057486e6023a17d7cd6a7f8f6f2dd58ff6fa
EhsanShahbazii <ehsan.shahbazipc@gmail.com>
2026-05-02 10:15:30 +0330

    style(ui): redesign dropdown container to match Antigravity native theme

---
+++ src/ui/components.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const ICONS = {
+    check: `<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 -960 960 960" fill="currentColor"><path d="M382-253.85L168.62-467.23L211.38-510L382-339.38L748.62-706l42.77,42.77L382-253.85Z"/></svg>`,
+    chevronRight: `<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 -960 960 960" fill="currentColor"><path d="M517.85-480l-184-184L376-706.15L602.15-480L376-253.85L333.85-296l184-184Z"/></svg>`,
+    arrowBack: `<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 -960 960 960" fill="currentColor"><path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z"/></svg>`
+};
+
+module.exports = { ICONS };
